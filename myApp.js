2ca34ebebf6804127882f890d3aec9ed30ef338e
pooja <poojakaruvan@gmail.com>
2025-06-25 18:31:33 +0300

    removebyid

--- myApp.js
+++ myApp.js
@@ -98,7 +98,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, person) => {
+    if(err) return done(err)
+    return done(null, person)
+  })
 };
 
 const removeManyPeople = (done) => {
