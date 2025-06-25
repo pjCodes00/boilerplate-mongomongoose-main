de3fb3b1c848c0c6ea5a519e45cddfe099e06e77
pooja <poojakaruvan@gmail.com>
2025-06-25 11:19:36 +0300

    findpersonbyid

--- myApp.js
+++ myApp.js
@@ -61,7 +61,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if(err) return done(err)
+      return done(null, data)
+  })
 };
 
 const findEditThenSave = (personId, done) => {
